8a4e0ca89e7392759273dda07c2322b456733a08
harshavardhank <harshavardhan.kalluru@alacriti.com>
2018-12-13 04:25:49 +0530

    added amcharts4 bar charts

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -4,6 +4,15 @@ import {CAChartConfig} from '../global/chartConfig';
 import {ActivatedRoute} from "@angular/router";
 import {ResultService} from "../_services/result.service";
 
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import am4themes_dark from "@amcharts/amcharts4/themes/animated";
+am4core.useTheme(am4themes_dark);
+am4core.useTheme(am4themes_animated);
+
+
+
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
@@ -288,6 +297,7 @@ export class ResultComponent implements OnInit, OnDestroy {
       // bar charts test
 
       this.drawBarChart();
+      this.drawBarChartsAm4();
 
       // this.setStockChart();
 
@@ -320,7 +330,112 @@ export class ResultComponent implements OnInit, OnDestroy {
   }
 
   drawBarChart() {
-        this.barChart1 = this.AmCharts.makeChart('bardiv1', this.barChartConfig1)
+        // this.barChart1 = this.AmCharts.makeChart('bardiv1', this.barChartConfig1)
+  }
+  drawBarChartsAm4() {
+
+// Themes end
+
+// Create chart instance
+      let chart = am4core.create("bardiv1", am4charts.XYChart);
+
+      let dupData = [{
+          "year": "2016",
+          "europe": 1.5,
+          "namerica": 1.5,
+          "asia": 1.1,
+          "lamerica": 2.3,
+          "meast": 1.2,
+          "africa": 1.1
+      }, {
+          "year": "2017",
+          "europe": 2.6,
+          "namerica": 2.7,
+          "asia": 2.2,
+          "lamerica": 0.3,
+          "meast": 0.3,
+          "africa": 0.1
+      }, {
+          "year": "2018",
+          "europe": 2.8,
+          "namerica": 2.9,
+          "asia": 2.4,
+          "lamerica": 0.3,
+          "meast": 0.3,
+          "africa": 0.1
+      }];
+// Add data
+      chart.data = [{
+          "year": "2016",
+          "europe": 2.5,
+          "namerica": 2.5,
+          "asia": 2.1,
+          "lamerica": 0.3,
+          "meast": 0.2,
+          "africa": 0.1
+      }, {
+          "year": "2017",
+          "europe": 2.6,
+          "namerica": 2.7,
+          "asia": 2.2,
+          "lamerica": 0.3,
+          "meast": 0.3,
+          "africa": 0.1
+      }, {
+          "year": "2018",
+          "europe": 2.8,
+          "namerica": 2.9,
+          "asia": 2.4,
+          "lamerica": 0.3,
+          "meast": 0.3,
+          "africa": 0.1
+      }];
+
+      chart.legend = new am4charts.Legend();
+      chart.legend.position = "right";
+
+// Create axes
+      let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = "year";
+      categoryAxis.renderer.grid.template.opacity = 0;
+
+      let valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+      valueAxis.min = 0;
+      valueAxis.renderer.grid.template.opacity = 0;
+      valueAxis.renderer.ticks.template.strokeOpacity = 0.5;
+      valueAxis.renderer.ticks.template.stroke = am4core.color("#495C43");
+      valueAxis.renderer.ticks.template.length = 10;
+      valueAxis.renderer.line.strokeOpacity = 0.5;
+      valueAxis.renderer.baseGrid.disabled = true;
+      valueAxis.renderer.minGridDistance = 40;
+
+// Create series
+      function createSeries(field, name) {
+          let series = chart.series.push(new am4charts.ColumnSeries());
+          series.dataFields.valueX = field;
+          series.dataFields.categoryY = "year";
+          series.stacked = true;
+          series.name = name;
+
+          let labelBullet = series.bullets.push(new am4charts.LabelBullet());
+          labelBullet.locationX = 0.5;
+          labelBullet.label.text = "{valueX}";
+          labelBullet.label.fill = am4core.color("#fff");
+      }
+
+      createSeries("europe", "Europe");
+      createSeries("namerica", "North America");
+      createSeries("asia", "Asia");
+      createSeries("lamerica", "Latin America");
+      createSeries("meast", "Middle East");
+      createSeries("africa", "Africa");
+
+
+      setTimeout(() => {
+          chart.data = dupData;
+          chart.validateData();
+      }, 3000)
+
   }
 
   drawFirstPieChartData () {
