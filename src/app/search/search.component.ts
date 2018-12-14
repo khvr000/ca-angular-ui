13eb82b65f19e63833ff08f4054047dcee072854
Akshay Raj <akshay.raj@alacriti.com>
2018-12-14 18:42:32 +0530

    pagination & glyphicon updated

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,6 +6,11 @@ import {SharedProperties} from '../_services/sharedProperties';
 import {MatPaginator, MatTableDataSource} from "@angular/material";
 import {Router} from "@angular/router";
 import {AlertService} from "../_services/alert.service";
+/*export interface PeriodicElement {
+    name: string;
+    weight: number;
+    symbol: string;
+}*/
 
 export interface PeriodicElement {
     time: string;
@@ -21,7 +26,59 @@ export interface PeriodicElement {
  {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'B'},
  {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'C'},
  {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'N'}*/
-let ELEMENT_DATA = [];
+// let ELEMENT_DATA = [];
+/*
+ {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+ {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+ {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+ {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+ {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+ {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+ {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+ {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+ {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+ {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+ {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+ {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+ {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+ {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+ {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+ {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
+ {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+ {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
+ {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
+ {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+*/
+/*const ELEMENT_DATA: PeriodicElement[] = [
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'H'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'He'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'Li'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'Be'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'B'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'C'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'N'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'B'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'C'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'N'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'B'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'C'},
+    {trackWord: 'Car', time: 'Dec 12 2018 15:21:50', View: 'Show', jobId: 'N'}
+];*/
+
+let ELEMENT_DATA: PeriodicElement[] = [
+/*    { name: 'Hydreeeogen', weight: 1.0079, symbol: 'H'},
+    { name: 'Helium', weight: 4.0026, symbol: 'He'},
+    { name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    { name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    { name: 'Boron', weight: 10.811, symbol: 'B'},
+    {name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    { name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    { name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+    {name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+    { name: 'Magnesium', weight: 24.305, symbol: 'Mg'}*/
+];
 
 @Component({
   selector: 'app-search',
@@ -31,8 +88,10 @@ let ELEMENT_DATA = [];
 export class SearchComponent implements OnInit {
 
     displayedColumns: string[] = ['trackWord', 'time', 'View'];
-    @ViewChild(MatPaginator) paginator: MatPaginator;
     dataSource: any;
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
     currentUser: User;
     users: User[] = [];
     loading = false;
@@ -48,9 +107,12 @@ export class SearchComponent implements OnInit {
                  private router: Router) {}
 
     ngOnInit() {
-        this.loading = false;
-        this.userDetails = JSON.parse(localStorage.getItem('userDetails'));
+        this.loading = true;
+        // this.dataSource.paginator = this.paginator;
 
+        this.userDetails = JSON.parse(localStorage.getItem('userDetails'));
+        // this.dataSource =  new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+       // this.dataSource.paginator = this.paginator;
         this.getJobDetails();
 
     }
@@ -61,7 +123,8 @@ export class SearchComponent implements OnInit {
            console.log( peopleArray);
            ELEMENT_DATA = peopleArray;
            console.log('Elements Data', ELEMENT_DATA);
-           this.dataSource = ELEMENT_DATA;
+           this.dataSource =  new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+           // this.dataSource = ELEMENT_DATA;
            this.dataSource.paginator = this.paginator;
            this.loading = true;
         });
@@ -78,6 +141,9 @@ export class SearchComponent implements OnInit {
             }
             this.saveJobId(receivedJobDetails);
         });
+    }
+    getTimeInFormat(time){
+        return Date.parse(time);
     }
     viewResult(element) {
         console.log(element);
